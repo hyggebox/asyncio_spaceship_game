7e9f2bede613c3cfab6221425089701882bf7f98
hyggebox <asya_apto@mail.ru>
2022-09-05 00:51:10 +0300

    Enable shooting after year 2020

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ async def animate_spaceship(canvas, frames):
         for _ in range(2):
             row_dir, col_dir, space_pressed = read_controls(canvas)
 
-            if space_pressed:
+            if year >= 2020 and space_pressed:
                 coroutines.append(fire(canvas,
                                        current_row,
                                        current_column + frame_cols // 2,
